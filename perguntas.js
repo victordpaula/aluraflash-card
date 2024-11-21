c5c86d72e49f282317ebb3defdf564ae817a7644
winner <0000108915771xsp@al.educacao.sp.gov.br>
2024-11-21 13:28:39 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'O Brasil',
+    'Qual a cidade mais populosa do Brasil?',
+    'São Paulo'
+)
+
+criaCartao(
+    'O Brasil',
+    'O Brasil é localizado em qual continente? ',
+    'América do Sul'
+)
+
+criaCartao(
+    'O Brasil',
+    'Quem foi o descobridor do Brasil?',
+    'Pedro Alvares Cabral'
+)
+
+criaCartao(
+    'O Brasil',
+    'Em que ano o Brasil foi descoberto?',
+    '1500'
+)
